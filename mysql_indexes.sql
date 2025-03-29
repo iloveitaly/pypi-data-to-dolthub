d90df379a056553f91b11878c7cba5dd4896af4f
Michael Bianco <mike@mikebian.co>
2025-03-29 07:30:38 -0600

    docs: comment out slow fulltext index on Dolthub
    this was much slower than a LIKE search on name
    
    Generated-by: aiautocommit

--- mysql_indexes.sql
+++ mysql_indexes.sql
@@ -1,5 +1,5 @@
 -- for most packages, first 20 chars is enough
 CREATE INDEX idx_name_prefix ON projects (name (20));
 
--- for the big guns, let's use full text
-ALTER TABLE projects ADD FULLTEXT INDEX idx_fulltext_name_summary (name);
+-- full text indexes are way too slow on dolthub
+-- ALTER TABLE projects ADD FULLTEXT INDEX idx_fulltext_name_summary (name);
